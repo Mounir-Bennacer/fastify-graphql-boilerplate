ed8340c67474d656887883312e362020c87da7a0
Mounir Bennacer <benphaoris@gmail.com>
2025-01-12 18:44:23 +0000

    feat: using import on any typeDefs or resolvers

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,34 +1,11 @@
 import { mergeTypeDefs, mergeResolvers } from "@graphql-tools/merge";
-import path from "path";
-import fs from "fs";
-import { DocumentNode } from "graphql";
-import { IResolvers } from "@graphql-tools/utils";
+import { userTypeDefs } from "@/modules/users/graphql/users.schema";
+import { userResolver } from "@/modules/users/graphql/users.resolver";
 
 export function loadModules() {
-  const modulesDir = path.join(__dirname);
+  const typeDefs = mergeTypeDefs([userTypeDefs]);
 
-  const typeDefsArray: (string | DocumentNode)[] = [];
-  const resolversArray: IResolvers[] = [];
-
-  const userModulePath = path.join(modulesDir, "users/graphql");
-
-  // Check if user module's typeDefs and resolvers exist and load them
-  const typeDefsPath = path.join(userModulePath, "user.typeDef.ts");
-  const resolversPath = path.join(userModulePath, "user.resolver.ts");
-
-  if (fs.existsSync(typeDefsPath)) {
-    const { userTypeDefs } = require(typeDefsPath);
-    typeDefsArray.push(userTypeDefs);
-  }
-
-  if (fs.existsSync(resolversPath)) {
-    const { userResolver } = require(resolversPath);
-    resolversArray.push(userResolver);
-  }
-
-  // Merge typeDefs and resolvers
-  const typeDefs = mergeTypeDefs(typeDefsArray);
-  const resolvers = mergeResolvers(resolversArray);
+  const resolvers = mergeResolvers([userResolver]);
 
   return { typeDefs, resolvers };
 }
