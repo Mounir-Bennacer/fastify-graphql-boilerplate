4227d9d764201f040f08639f199fdcfa079050b7
Mounir Bennacer <benphaoris@gmail.com>
2025-01-12 18:45:51 +0000

    feat: correcting a duplication in starting the server

--- src/server.ts
+++ src/server.ts
@@ -1,27 +1,3 @@
-import { db } from "@/db";
-import { ApolloServer } from "@apollo/server";
-import { config } from "@config";
-import Fastify from "fastify";
-import { Client } from "pg";
-import { loadModules } from "@/modules";
-// import { fastifyApollo } from "@as-integrations/fastify";
-
-const envToLogger = {
-  local: {
-    transport: {
-      target: "pino-pretty",
-      options: {
-        translateTime: "SYS:standard",
-        ignore: "pid,hostname",
-      },
-    },
-  },
-  production: true,
-  test: false,
-} as const;
-
-type EnvKeys = keyof typeof envToLogger;
-
 const startServer = async (): Promise<void> => {
   const server = Fastify({ logger: envToLogger[config.APP_ENV as EnvKeys] });
 
@@ -29,18 +5,9 @@ const startServer = async (): Promise<void> => {
     connectionString: config.DB_URL,
   });
 
-  // const context = () => {
-  //   return {
-  //     db,
-  //   };
-  // };
-
   try {
     await dbClient.connect();
-
     server.log.info("Connected to database successfully");
-
-    await server.listen({ port: config.APP_PORT });
 
     const { typeDefs, resolvers } = loadModules();
 
@@ -52,18 +19,18 @@ const startServer = async (): Promise<void> => {
 
     await apolloServer.start();
 
+    // Uncomment the following lines if you want to register Apollo Server with Fastify
     // server.register(fastifyApollo(apolloServer), {
     //   path: "/graphql",
     // });
 
     await server.listen({ port: config.APP_PORT });
-    server.log.info(`** Server listening on port ** ${config.APP_PORT}`);
+    server.log.info(`Server listening on port ${config.APP_PORT}`);
   } catch (error) {
     console.error({ error });
     server.log.error(`Database connection failed: ${error}`);
 
     await dbClient.end();
-
     process.exit(1);
   }
 };
